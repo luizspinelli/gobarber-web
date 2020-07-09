5223e03a87cb382ec7215ccd6191266ff9f42b5a
luiz Spinelli <luiz.lucchi@gmail.com>
2020-07-09 12:59:55 -0300

    spliting components

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Container, Content, Background } from './styles';
 import logo from '../../assets/logo.svg';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const Signin: React.FC = () => (
   <Container>
@@ -9,9 +11,9 @@ const Signin: React.FC = () => (
       <img src={logo} alt="GoBarber" />
       <form>
         <h1>Faça seu logon</h1>
-        <input placeholder="Email" />
-        <input type="password" placeholder="Senha" />
-        <button type="submit">Entrar</button>
+        <Input name="email" icon={FiMail} placeholder="Email" />
+        <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+        <Button type="submit">Entrar</Button>
         <a href="forgot">Esqueci minha senha</a>
       </form>
 
