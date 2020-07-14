2372cceb3207646ca51c6484bf86e3b00789d6dc
luiz Spinelli <luiz.lucchi@gmail.com>
2020-07-14 20:21:26 -0300

    authenticated routes

---
+++ src/routes/Route.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-props-no-spreading */
+import React from "react";
+import {
+  RouteProps as RouteDomProps,
+  Route as ReactDomRoute,
+  Redirect,
+} from "react-router-dom";
+import { useAuth } from "../hooks/auth";
+
+interface RouteProps extends RouteDomProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+const Route: React.FC<RouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+  return (
+    <ReactDomRoute
+      {...rest}
+      render={() => {
+        return isPrivate === !!user ? (
+          <Component />
+        ) : (
+          <Redirect to={{ pathname: isPrivate ? "/" : "/dashboard" }} />
+        );
+      }}
+    />
+  );
+};
+
+export default Route;
