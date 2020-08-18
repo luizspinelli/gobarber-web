25c536537bd69efdb3003a6878ef9732c78f79dd
Luiz Spinelli <luiz.lucchi@gmail.com>
2020-08-17 21:45:38 -0300

    create NextAppointment

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 
-import { FiPower } from "react-icons/fi";
+import { FiPower, FiClock } from "react-icons/fi";
 
 import logoImg from "../../assets/logo.svg";
 
-import { Container, Header, HeaderContent, Profile } from "./styles";
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from "./styles";
 import { useAuth } from "../../hooks/auth";
 
 const Dashboard: React.FC = () => {
@@ -16,7 +25,10 @@ const Dashboard: React.FC = () => {
         <HeaderContent>
           <img src={logoImg} alt="GoBarber" />
           <Profile>
-            <img src={user.avatar_url} alt={user.name} />
+            <img
+              src="https://avatars3.githubusercontent.com/u/57016585?s=460&u=5e21f897d2ab550c9ac2ca6abdb58d7fde8f67cb&v=4"
+              alt={user.name}
+            />
             <div>
               <span>Bem Vindo</span>
               <strong>{user.name}</strong>
@@ -27,6 +39,32 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários Agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-Feira</span>
+          </p>
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars3.githubusercontent.com/u/57016585?s=460&u=5e21f897d2ab550c9ac2ca6abdb58d7fde8f67cb&v=4"
+                alt="Luiz Spinelli"
+              />
+              <strong>Luiz Spinelli</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
